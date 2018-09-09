0ef177ca1630007c57632abd07aec277dfe73e66
Angelica Costantini <angelicacostantini07@gmail.com>
2018-09-09 13:55:13 +0200

    adding some CSS

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -2,10 +2,10 @@ function FizzBuzz() {
     this.check = (number) => {
         if (number % 3 === 0 && number % 5 === 0){
             return 'FizzBuzz';
-        } else if(number % 3 === 0) {
-            return 'Fizz';
-        } else if (number % 5 === 0) {
-            return 'Buzz';   
+        } else if(number % 5 === 0) {
+            return 'Buzz';
+        } else if (number % 3 === 0) {
+            return 'Fizz';   
         } else {
             return number
         }
